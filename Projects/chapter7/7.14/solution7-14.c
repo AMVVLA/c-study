7599b2d1c6ea0cd5fcd1998ceb55b7658543328b
AMVVLA <superego0706@naver.com>
2020-04-07 21:38:19 +0900

    works

--- Projects/chapter7/7.14/solution7-14.c
+++ Projects/chapter7/7.14/solution7-14.c
@@ -3,17 +3,16 @@
 
 int main(void) {
 
-    double number, x = 1;
+    double number, x; 
+	x = 1;
     int i = 0;
 
     printf("Enter a positive number: ");
     scanf("%lf", &number);
 
-    while (fabs(number - x) > 0.00001)
+    while (fabs(number/x - x) > 0.00001)
         x = (x + number / x) / 2;
 
     printf("Square root: %.5f\n", x);
-
-    printf("%d\n", i);
     return 0;
 }
