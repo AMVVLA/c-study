bcfe9d18815e8efd0c5629ece13b64038b52a871
AMVVLA <superego0706@naver.com>
2020-04-18 19:26:59 +0900

    8.04

--- Projects/Chapter8/8.04/solution8.04.c
+++ Projects/Chapter8/8.04/solution8.04.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#define N (int)(sizeof(a) / sizeof(a[0]))
+#define N 10
+#define SIZE (int)(sizeof(a) / sizeof(a[0]))
 
 int main()
 {
 	int a[N], i;
 
-	printf("Enter %d numbers: ", N);
-	for(i = 0; i < N; i++)
+	printf("Enter %d numbers: ", SIZE);
+	for(i = 0; i < SIZE; i++)
 		scanf("%d", &a[i]);
 	printf("In reverse order:");
-	for(i = N - 1; i >= 0; i--)
+	for(i = SIZE - 1; i >= 0; i--)
 		printf("%d", a[i]);
 	printf("\n");
 	return 0;
